013e45470c223c996b8ac390ac0608ac4ee09621
rycu <rycuMMXVII@cuttermail.co.uk>
2017-05-25 22:06:16 +0100

    removed merge backups

--- cls/MailParts.class_LOCAL_13119.php
+++
@@ -1,6 +0,0 @@
-<?
-class MailParts{	
-	
-	public $footer = "Information contained in this e-mail is intended for the use of the addressee only, is confidential and may be legally privileged. Any further dissemination, distribution, copying or use of this communication without prior permission of the sender is strictly prohibited. The contents of any attachments to this e-mail may contain software viruses, which could cause damage to your own computer system.<br /><br />Whilst every reasonable precaution has been taken to minimise this risk, we cannot accept liability for any damage, which you sustain as a result of software viruses. You should therefore carry out your own virus checks before opening the attachment.";
-}
-?>
