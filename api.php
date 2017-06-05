8e056b2fe297bb06e0095c48f0fca9ba54017a0a
rycu <rycuMMXVII@cuttermail.co.uk>
2017-06-05 19:02:46 +0100

    comments added to api

--- api.php
+++ api.php
@@ -5,6 +5,7 @@ spl_autoload_register(function ($class){
 
 function validateValue($name, $value) {
 
+	//REGEX can be added to any field name
 	$regexListArr = array (
 		"name" => "\\S",
 		'email' => "[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,6})",
@@ -13,6 +14,7 @@ function validateValue($name, $value) {
 		
 	$cleared = true;
 
+	//check each field against regex array
 	if(isset($regexListArr[$name])){
 		if (!preg_match ('/'.$regexListArr[$name].'/', $value)){
 			$cleared = false;
@@ -21,14 +23,33 @@ function validateValue($name, $value) {
 	return $cleared;
 }
 
+function sendMail($inputs){
+	
+	//TEMP EMAIL ACC from maildrop.cc
+	$mailaddress = 'near_perfection@maildrop.cc';
+	$mailsubject = 'Message from the '.$_SERVER['SERVER_NAME'].' Contact Form';
+	$mailMsg = "<p>".$mailsubject."</p>";
+
+	//add each field
+	foreach ($inputs as $key => $value) {
+		$mailMsg .= "<p>".$key.": ".$value."</p>";
+	}
+
+	//send fields to master mail template
+	$mailer = new Mail();
+	$mailer->sendMasterTemplate($mailaddress, $mailsubject, $mailMsg);
+
+	return true;
+}
+
 
 if($_POST['task'] == 'quickContactSend'){
 
 	$errorListArr = array();
 	$responceArr = array();
-
 	$cleared = true;
 
+	// Build Error list if required
 	foreach (json_decode($_REQUEST["inputs"]) as $key => $value) {
 		
 		if(!validateValue($key, $value)){
@@ -37,39 +58,17 @@ if($_POST['task'] == 'quickContactSend'){
 		}
 	}
 
+	// proceed with send mail if validation is cleared
 	if($cleared){
 		if(sendMail(json_decode($_REQUEST["inputs"]))){
-
 			echo json_encode('sent');
 		}
-		
 	}else{
+		//return error list
 		echo json_encode($errorListArr);
 	}
  }
 
-
-function sendMail($inputs){
-	
-	//TEMP EMAIL ACC from maildrop.cc
-	$mailaddress = 'near_perfection@maildrop.cc';
-	
-	$mailsubject = 'Message from the '.$_SERVER['SERVER_NAME'].' Contact Form';
-	
-	$mailMsg = "<p>".$mailsubject."</p>";
-
-	foreach ($inputs as $key => $value) {
-		$mailMsg .= "<p>".$key.": ".$value."</p>";
-	}
-
-	$mailer = new Mail();
-	
-	$mailer->sendMasterTemplate($mailaddress, $mailsubject, $mailMsg);
-
-	return true;
-
-}
 			
 die();
-
 ?>
